f52dcc0dc5f4e56579938466a62b56a9ca134dc3
timebigot <pid@shinjustin.com>
2021-11-03 23:02:30 -0400

    Updated imdb_api to also retrieve rating, director, cast, release date, plot, genre

--- utils/imdb_api.py
+++ utils/imdb_api.py
@@ -2,19 +2,24 @@ import imdb
 import re
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def imdb_api(query):
     data = {
-            "id": None,
+            "tid": None,
             "title": None,
             "poster": None,
             "rating": None,
+            "director": None,
+            "cast": None,
+            "plot": None,
+            "release": None,
+            "genre": None,
             }
     url = f"https://www.imdb.com/find?q={query}&s=tt&ttype=ft&ref_=fn_ft"
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
 
-    ia = imdb.IMDb()
     search_table = soup.find("table", class_="findList")
 
     # return first movie on list
@@ -30,14 +35,74 @@ def imdb_api(query):
     tiny_poster = anchor.find("img")["src"]
     poster = re.sub("\_.*\_\.", "", tiny_poster)
 
+    # now use imdb api to get the rest
+    ia = imdb.IMDb()
+
+    # api does not like the tt prefix in id
+    imdb_id = tid.replace("tt", "")
+    mov = ia.get_movie(imdb_id)
+
+    # get people
+    director = mov['directors'][0]['name']
+    cast = list()
+    for person in mov['cast'][0:5]:
+        cast_name = person['name']
+        cast_role = person.currentRole['name']
+        person_id = person.getID()
+        cast_member = ia.get_person(person_id)
+        tiny_cast_image = cast_member['headshot']
+        cast_image = re.sub("\_.*\_\.", "", tiny_cast_image)
+        cast.append(
+                {
+                    'name': cast_name,
+                    'role': cast_role,
+                    'image': cast_image
+                    }
+                )
+
+    # get rating e.g. PG-13
+    certs = mov['certificates']
+    rating = None
+    for cert in certs:
+        if(cert == "United States:G"):
+            rating = "G"
+            break
+        elif(cert == "United States:PG"):
+            rating = "PG"
+            break
+        elif(cert == "United States:PG-13"):
+            rating = "PG-13"
+            break
+        elif(cert == "United States:R"):
+            rating = "R"
+            break
+
+    # get the release date
+    raw_date = mov['original air date']
+    raw_date = raw_date.replace(" (USA)", "")
+    release = datetime.strptime(raw_date, "%d %b %Y").timestamp()
+    release = int(release)
+
+    # get the plot
+    plot = mov['plot outline']
+
+    # get the genre
+    genre = mov['genre']
+
     data["tid"] = tid
     data["title"] = title
     data["poster"] = poster
+    data["director"] = director
+    data["cast"] = cast
+    data["rating"] = rating
+    data["plot"] = plot
+    data["genre"] = genre
+    data["release"] = release
 
     return data
 
 if __name__ == "__main__":
     from pprint import pprint
-    query = "not time to die"
+    query = "dune"
     data = imdb_api(query)
     pprint(data)
